53780958909fad808bbf2b182426c3128de7469e
blackrebel <72fabio1@virgilio.it>
2017-10-12 17:09:13 +0200

    [1] Add an option for use prebuilt webview/chromium lib [2] Add android.hardware.bluetooth.xml to permissions [3] Add libdpframework

--- device_g700.mk
+++ device_g700.mk
@@ -23,6 +23,16 @@ else
 	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
 endif
 
+# Hack for use prebuilt webviewchromium
+ifeq ($(PRODUCT_PREBUILT_WEBVIEWCHROMIUM),yes)
+PRODUCT_COPY_FILES += \
+        $(call find-copy-subdir-files,*,vendor/huawei/g700/proprietary/framework,system/framework)
+PRODUCT_COPY_FILES +=  \
+        vendor/huawei/g700/proprietary/lib/libchromium_net.so:system/lib/libchromium_net.so \
+        vendor/huawei/g700/proprietary/lib/libwebviewchromium.so:system/lib/libwebviewchromium.so \
+        vendor/huawei/g700/proprietary/lib/libwebviewchromium_plat_support.so:system/lib/libwebviewchromium_plat_support.so
+endif
+
 # Ramdisk
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/rootdir/fstab.mt6589:root/fstab.mt6589 \
@@ -31,12 +41,14 @@ PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/rootdir/ueventd.mt6589.rc:root/ueventd.mt6589.rc \
 	$(LOCAL_PATH)/rootdir/init.protect.rc:root/init.protect.rc \
 	$(LOCAL_PATH)/rootdir/twrp.fstab:recovery/root/etc/twrp.fstab \
-	$(LOCAL_PATH)/rootdir/init.mt6589.usb.rc:/root/init.mt6589.usb.rc
+	$(LOCAL_PATH)/rootdir/init.mt6589.usb.rc:/root/init.mt6589.usb.rc \
+	$(LOCAL_PATH)/rootdir/sbin/ksh:root/sbin/ksh \
+	$(LOCAL_PATH)/rootdir/sbin/bb:root/sbin/bb
 
 # Permissions
-
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+        frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
 	frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
 	frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
 	frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
@@ -45,9 +57,17 @@ PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
 	frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
 	frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
-	frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml
+	frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+	frameworks/native/data/etc/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml
+	
 
-
+# build.prop	
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+        ro.adb.secure=0 \
+        ro.mount.fs=EXT4 \
+        ro.secure=0 \
+        ro.allow.mock.location=1
+        
 PRODUCT_PROPERTY_OVERRIDES := \
 	fmradio.driver.chip=3 \
 	gps.solution.combo.chip=1 \
@@ -87,13 +107,13 @@ PRODUCT_PROPERTY_OVERRIDES := \
 
 PRODUCT_TAGS += dalvik.gc.type-precise
 
+# Ril
 PRODUCT_PACKAGES += \
 	gsm0710muxd
 
 # Wi-Fi
 PRODUCT_PACKAGES += \
 	lib_driver_cmd_mtk
-
 
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/configs/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
@@ -104,25 +124,28 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
 	audio.r_submix.default \
 	audio.a2dp.default \
-    audio.usb.default \
+        audio.usb.default \
 	libblisrc \
-    libdashplayer \
-    libxlog
+        libdashplayer \
+        libxlog
+        
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
-	$(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profile.xml
+	$(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
 
 # PowerVR SGX544 GPU-Related
 PRODUCT_PACKAGES += \
-	libcorkscrew
-
+	libcorkscrew \
+        gralloc_extra
+        
+PRODUCT_COPY_FILES += \
+	vendor/huawei/g700/proprietary/lib/libdpframework.so:obj/lib/libdpframework.so \
+	vendor/huawei/g700/proprietary/lib/libdpframework_os.so:obj/lib/libdpframework_os.so \
+	vendor/huawei/g700/proprietary/lib/libdpframework_plat.so:obj/lib/libdpframework_plat.so
+		       	       	       
 # Build Torch
 PRODUCT_PACKAGES += \
 	Torch
-
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/rootdir/sbin/ksh:root/sbin/ksh \
-	$(LOCAL_PATH)/rootdir/sbin/bb:root/sbin/bb
 
 $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
 
